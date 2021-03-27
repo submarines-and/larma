7b3c234610e48ef842542696b98ccb0a9b00a81a
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2021-03-27 16:12:43 +0100

    Initial Commit

---
+++ Larma/LarmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarmaApp.swift
+//  Larma
+//
+//  Created by Johannes Sörensen on 2021-03-27.
+//
+
+import SwiftUI
+
+@main
+struct LarmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
